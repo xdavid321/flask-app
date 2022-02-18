66613c309a0c402c6fbe40a6b51bc17698585181
SRINIDHI <srinidhi@SRINIDHIs-MacBook-Air.local>
2022-02-17 21:52:26 -0500

    AWS changes

--- rds_db.py
+++ rds_db.py
@@ -2,25 +2,25 @@ import pymysql
 #import aws_credentials as rds
 
 conn = pymysql.connect(
-        host= "database-1.cfmqfilrkvc8.us-east-2.rds.amazonaws.com",
+      host= "database-3.c6ojy4yqq7gm.us-east-2.rds.amazonaws.com",
         user = "admin",
-        password = "kingmaker007",
+        password = "Kingmaker007",
         port = 3306
         )
 
 def insert_details(fName, lName,email,password):
     cur=conn.cursor()
-    cur.execute("INSERT INTO SRINIDHI_WORKSPACE.LOGIN_INFO(FIRST_NAME,LAST_NAME,EMAIL,PASSWORD) VALUES (%s,%s,%s,%s)", (fName, lName,email,password))
+    cur.execute("INSERT INTO AWS_TEAM8_DATABASE.USER_REGISTRATION_INFO(FIRST_NAME,LAST_NAME,EMAIL,PASSWORD) VALUES (%s,%s,%s,%s)", (fName, lName,email,password))
     conn.commit()
 
 def get_details():
     cur=conn.cursor()
-    cur.execute("SELECT *  FROM SRINIDHI_WORKSPACE.LOGIN_INFO")
+    cur.execute("SELECT *  AWS_TEAM8_DATABASE.USER_REGISTRATION_INFO")
     details = cur.fetchall()
     return details
 
 def get_email_password_details():
     cur=conn.cursor()
-    cur.execute("SELECT EMAIL,PASSWORD FROM SRINIDHI_WORKSPACE.LOGIN_INFO")
+    cur.execute("SELECT EMAIL,PASSWORD FROM AWS_TEAM8_DATABASE.USER_REGISTRATION_INFO")
     User_deatils = cur.fetchall()
     return User_deatils
